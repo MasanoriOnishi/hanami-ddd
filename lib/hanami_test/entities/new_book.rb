42a7e4e4510b52ef68ec1eacc39f25be6cbf8f6f
MasanoriOnishi <monarisamasa@gmail.com>
2022-04-24 22:56:28 +0900

    Refactor repository inner logic

--- lib/hanami_test/entities/new_book.rb
+++ lib/hanami_test/entities/new_book.rb
@@ -1,6 +1,6 @@
 class NewBook < Hanami::Entity
   MAX_TITLE_SIZE = 20
-  def self.create(id:, title:, author: "作者不明", created_at: Time.now, updated_at: Time.now)
+  def self.create(id:, title:, author: "作者不詳", created_at: Time.now, updated_at: Time.now)
     if title.size >= MAX_TITLE_SIZE
       raise ArgumentError, "titleは#{MAX_TITLE_SIZE}文字までです。"
     end
--- lib/hanami_test/repositories/new_book_repository.rb
+++ lib/hanami_test/repositories/new_book_repository.rb
@@ -1,11 +1,5 @@
 class NewBookRepository < Hanami::Repository
   def store(book)
-    new_books.insert(
-      id: book.id,
-      title: book.title,
-      author: book.author,
-      created_at: book.created_at,
-      updated_at: book.updated_at,
-    )
+    new_books.insert(book.to_h)
   end
 end
