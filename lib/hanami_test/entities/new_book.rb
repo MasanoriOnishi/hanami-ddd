e50e22064b68f38031fbeadefd99cfc143216a44
MasanoriOnishi <monarisamasa@gmail.com>
2022-04-24 22:49:22 +0900

    Add validation for book entity

--- lib/hanami_test/entities/new_book.rb
+++ lib/hanami_test/entities/new_book.rb
@@ -1,5 +1,10 @@
 class NewBook < Hanami::Entity
-  def self.create(id:, title:, author:, created_at: Time.now, updated_at: Time.now)
+  MAX_TITLE_SIZE = 20
+  def self.create(id:, title:, author: "作者不明", created_at: Time.now, updated_at: Time.now)
+    if title.size >= MAX_TITLE_SIZE
+      raise ArgumentError, "titleは#{MAX_TITLE_SIZE}文字までです。"
+    end
+
     self.new(
       id: id,
       title: title,
